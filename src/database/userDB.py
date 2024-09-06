b771352f78ac7b40a68d903895fcb12e1f886560
Nieva Lucas <nievalucas60@gmail.com>
2024-09-06 12:38:03 -0300

    Quitado de formato Row en conexion a BBDD

--- src/database/userDB.py
+++ src/database/userDB.py
@@ -4,7 +4,6 @@ from flask import g
 def get_infoUsers_db():
     if 'db' not in g:
         g.db = sqlite3.connect('/data/infoUser.db')
-        g.db.row_factory = sqlite3.Row
     return g.db
 
 def close_infoUsers_db():
