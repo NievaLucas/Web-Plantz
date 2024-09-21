dd6e497edfcd09d4a7b3a6c21e2a795f7ca2503f
Nieva Lucas <nievalucas60@gmail.com>
2024-09-21 00:31:29 -0300

    Cambio de ubicacion en los methods

--- src/__init__.py
+++ src/__init__.py
@@ -6,8 +6,8 @@ app = Flask(__name__, template_folder="templates")
 
 def createApp() :
     
-    app.register_blueprint(routeRegister.main, methods = ["POST"], url_prefix='/Registro')
-    app.register_blueprint(routeLogin.main, methods = ["POST"], url_prefix='/IniciarSesion')
+    app.register_blueprint(routeRegister.main, url_prefix='/Registro')
+    app.register_blueprint(routeLogin.main, url_prefix='/IniciarSesion')
     app.register_blueprint(routeStatistic.main, methods = ["GET", "POST"], url_prefix='/Estadisticas')
 
     return app
