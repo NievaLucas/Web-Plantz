57697dc2a013d949ce106bd518d7c0d7552b9aa2
Nieva Lucas <nievalucas60@gmail.com>
2024-09-22 21:33:37 -0300

    Loggin sin comprobacion de HASH

--- src/routes/routeLogin.py
+++ src/routes/routeLogin.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
+from src.database.userDB import connUser
 
 main = Blueprint("login_blueprint", __name__)
 
@@ -7,7 +8,15 @@ def login():
     if request.method == "POST" :
         user = request.form["userDB"]
         password = request.form["passwordDB"]
+        date = (user, password)
 
-        return redirect(url_for('statistic_blueprint.esp32'))
+        cursor = connUser.cursor()
+        sql = "SELECT Usuario, Contraseña FROM usuarios WHERE Usuario = %s AND Contraseña = %s"
+        cursor.execute(sql, (date))
+        account = cursor.fetchall()
+
+        if account :
+            return redirect(url_for('statistic_blueprint.esp32'))
+        
     else :
         return render_template('auth/login.html')
