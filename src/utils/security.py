4d61f49d99b8eae6fe515dc1f7fba0561a259329
Nieva Lucas <nievalucas60@gmail.com>
2024-09-23 00:25:00 -0300

    Se añadio funcion de check hash

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,5 +1,8 @@
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 def hash_password(newPassword) :
     password = generate_password_hash(newPassword)
     return password
+
+def check_password(encoded, password) :
+    check_password_hash(encoded, password)
