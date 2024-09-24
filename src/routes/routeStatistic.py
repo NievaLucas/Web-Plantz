b9c0f0c89d6377538d82a173afb5b35e46cc0708
Nieva Lucas <nievalucas60@gmail.com>
2024-09-24 12:55:39 -0300

    Backend de estadisitica (version de prueba)

--- src/routes/routeStatistic.py
+++ src/routes/routeStatistic.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from src.database.infoEspDB import connEsp32
 
 main = Blueprint("statistic_blueprint", __name__)
 
-@main.route('')
+@main.route('', methods = ["GET", "POST"],)
 def esp32():
-    return render_template('statistic.html')
+
+    if request.method == "POST" :    
+
+        idProducto = request.form["idEsp32"] 
+    
+        cursorEsp32 = connEsp32.cursor()
+        sql = "SELECT Hora, Temperatura, Humedad FROM esp32 WHERE idProducto = %s"    
+        cursorEsp32.execute(sql, (idProducto, ))
+        datosEsp32 = cursorEsp32.fetchall()
+        
+        datoslal = ("sas", "sas", "sas")
+        if idProducto == "sas" :
+            return render_template('statistic.html', datosEsp32 = datoslal)
+    
+    else :
+        return render_template('statistic.html')
