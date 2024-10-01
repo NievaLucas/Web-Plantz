9fe99e2267c2026c7a5ea4d705b6fa7b3c388214
Nieva Lucas <nievalucas60@gmail.com>
2024-10-01 00:01:07 -0300

    Cambio en conexion a BBDD de local a la nube

---
+++ src/database/conectDB.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+
+    host = "sql10.freemysqlhosting.net",
+    user = "sql10734546",
+    password = "1BHCaGxmuw",
+    port = "3306",
+    database = "sql10734546"
+
+)
