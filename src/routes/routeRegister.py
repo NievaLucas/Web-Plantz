270b9bb6b8caf6338a4f931b5c71a83e6c808cbf
Nieva Lucas <nievalucas60@gmail.com>
2024-10-09 00:27:56 -0300

    Documentacion por sector del codigo

--- src/routes/routeRegister.py
+++ src/routes/routeRegister.py
@@ -1,28 +1,38 @@
+# Componentes que se utilizaran
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from src.database.conectDB import db
 from src.utils.security import hash_password
 
+# Definicion del Blueprint
 main = Blueprint("registers_blueprint", __name__)
 
+# Ruta y los metodos permitidos
 @main.route('', methods = ["GET", "POST"])
 def registers() :
+    # Si el metodo del formulario es POST
     if request.method == "POST":
+        # Se obtienen los datos del formulario
         name = request.form["name"]
         surname = request.form["surname"]
         gmail = request.form["gmail"]
         newUser = request.form["user"]
         newPassword = request.form["password"]
-        insertToBD = (name, surname, gmail, newUser, hash_password(newPassword))
-        
-        for i in insertToBD :
-            if i == "" :
-                flash("Llenar todos los campos")
-                return render_template('/auth/register.html')
-            else :
-                sql = "INSERT INTO usuarios (Nombre, Apellido, Gmail, Usuario, Contraseña) VALUES (%s, %s, %s, %s, %s)"
-                cursor = db.cursor()
-                cursor.execute(sql, (insertToBD))
-                db.commit()
-                return redirect(url_for('statistic_blueprint.esp32'))
-        
+        # Tupla a evaluar
+        valueNull = (name, surname, gmail, newUser, newPassword)
+        # Si algun dato esta vacio
+        if "" in valueNull :
+            # Si devuelve verdadero notificamos el error
+            flash("Todos los campos son obligatorios")
+            return render_template('/auth/register.html')
+        else :
+            # Tupla a insertar en la base de datos
+            insertToDB = (name, surname, gmail, newUser, hash_password(newPassword))
+            # Sentencias de la base de datos
+            sql = "INSERT INTO usuarios (Nombre, Apellido, Gmail, Usuario, Contraseña) VALUES (%s, %s, %s, %s, %s)"
+            cursor = db.cursor()
+            cursor.execute(sql, (insertToDB))
+            db.commit()
+            # Redireccionamos a la ruta de estadisticas si el registro fue exitoso
+            return redirect(url_for('statistic_blueprint.esp32'))
+    # Si el metodo es GET renderizamos la plantilla
     return render_template('/auth/register.html')
