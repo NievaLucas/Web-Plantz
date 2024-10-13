d11b6b4984a48ea10eb26913e7ef271f060645a0
Nieva Lucas <nievalucas60@gmail.com>
2024-10-13 19:54:13 -0300

    Proteccion contra ataques CSRF

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,14 @@
 # Componentes que se utilizaran
 from decouple import config
 from flask import Flask, render_template
+from flask_wtf import CSRFProtect
 from src.routes import routeLogin, routeRegister, routeStatistic
 
 # Declaracion de app
 app = Flask(__name__, template_folder="templates")
+
+# Componente con el cual protegeremos de ataques CSRF
+csrf = CSRFProtect()
 
 def createApp() :
 
@@ -15,6 +19,9 @@ def createApp() :
 
     # Configuracion de llave secreta
     app.secret_key = config('SECRET_KEY')
+
+    # Inicializador de la proteccion
+    csrf.init_app(app)
 
     return app
 
