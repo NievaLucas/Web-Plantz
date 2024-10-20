5de40284ad98259b80ec6dccc95d8c6f275cc08a
Nieva Lucas <nievalucas60@gmail.com>
2024-10-20 13:06:13 -0300

    Documentacion del codigo

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,7 +1,9 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# Funcion con la que generamos un nuevo Hash
 def hash_password(newPassword) :
     return generate_password_hash(newPassword)
 
+# Funcion con la que verificamos que la contraseña hasheada y el texto plano son las mismas
 def check_password(encoded, password) :
     return check_password_hash(encoded, password)
