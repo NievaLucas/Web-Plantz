f5487e10f9ff5052c42394c0cec4f96a5ec4b851
Nieva Lucas <nievalucas60@gmail.com>
2024-11-08 14:49:48 -0300

    Añadido de flask-wtf

--- src/routes/routeRegister.py
+++ src/routes/routeRegister.py
@@ -1,6 +1,7 @@
 # Componentes que se utilizaran
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from src.database.conectDB import db
+from src.utils.forms import Registro
 from src.utils.security import hash_password
 
 # Definicion del Blueprint
@@ -9,33 +10,22 @@ main = Blueprint("registers_blueprint", __name__)
 # Ruta y los metodos permitidos
 @main.route('', methods = ["GET", "POST"])
 def registers() :
-    # Si el metodo del formulario es POST
-    if request.method == "POST":
-        # Se obtienen los datos del formulario
-        name = request.form["name"]
-        surname = request.form["surname"]
-        gmail = request.form["gmail"]
-        newUser = request.form["user"]
-        newPassword = request.form["password"]
-        # Tupla a evaluar
-        valueNull = (name, surname, gmail, newUser, newPassword)
-        # Si algun dato esta vacio
-        if "" in valueNull :
-            # Si devuelve verdadero notificamos el error
-            flash("Todos los campos son obligatorios")
-            return render_template('/auth/register.html')
-        else :
-            # Tupla a insertar en la base de datos
-            # Con la funcion "hash_password" creamos el hash para la nueva contraseña
-            insertToDB = (name, surname, gmail, newUser, hash_password(newPassword))
-            # Cursor para manejar la base de datos
-            cursor = db.cursor()
-            # Sentencia SQL
-            sql = "INSERT INTO usuarios (Nombre, Apellido, Gmail, Usuario, Contraseña) VALUES (%s, %s, %s, %s, %s)"
-            cursor.execute(sql, (insertToDB)) # Insertamos la tupla en la base de datos
-            db.commit()
-            # Redireccionamos a la ruta de estadisticas si el registro fue exitoso
-            return redirect(url_for('login_blueprint.login'))
+    form = Registro()
+    if form.validate_on_submit() :
+        
+        name = form.name.data
+        surname = form.surname.data
+        gmail = form.gmail.data
+        username = form.user.data
+        passwordHash = hash_password(form.password.data)
+
+        cursor = db.cursor()
+        sql = """INSERT INTO usuarios (Nombre, Apellido, Gmail, Usuario, Contraseña) 
+                VALUES (%s, %s, %s, %s, %s)"""
+        cursor.execute(sql, (name, surname, gmail, username, passwordHash))
+        db.commit()
+       
+        return redirect(url_for('login_blueprint.login'))
     # Si el metodo es GET renderizamos la plantilla
     else :
-        return render_template('/auth/register.html')
+        return render_template('/auth/register.html', form = form)
