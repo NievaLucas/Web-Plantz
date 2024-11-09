2fdb5768ae4c70e9e842d5179c6d3be07bb179e2
Nieva Lucas <nievalucas60@gmail.com>
2024-11-09 01:31:54 -0300

    Documentacion del codigo

--- src/routes/routeRegister.py
+++ src/routes/routeRegister.py
@@ -1,5 +1,5 @@
 # Componentes que se utilizaran
-from flask import Blueprint, request, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for
 from src.database.conectDB import db
 from src.utils.forms import Registro
 from src.utils.security import hash_password
@@ -10,22 +10,29 @@ main = Blueprint("registers_blueprint", __name__)
 # Ruta y los metodos permitidos
 @main.route('', methods = ["GET", "POST"])
 def registers() :
+    # Traemos la clase Registro, la cual contiene el formulario
     form = Registro()
+    # Si el submit del formulario es valido (Es decir: True)
     if form.validate_on_submit() :
         
+        # Obtenemos cada dato del formulario
         name = form.name.data
         surname = form.surname.data
         gmail = form.gmail.data
         username = form.user.data
+        # Hasheado de contraseña
         passwordHash = hash_password(form.password.data)
 
         cursor = db.cursor()
+        # Sentecia SQL con la que ingresamos el nuevo registro a la base de datos
         sql = """INSERT INTO usuarios (Nombre, Apellido, Gmail, Usuario, Contraseña) 
                 VALUES (%s, %s, %s, %s, %s)"""
         cursor.execute(sql, (name, surname, gmail, username, passwordHash))
         db.commit()
        
+        # Redireccionamos a Login
         return redirect(url_for('login_blueprint.login'))
-    # Si el metodo es GET renderizamos la plantilla
+    
+    # Sino, renderizamos la plantilla y el formulario que contiene la clase Registro
     else :
         return render_template('/auth/register.html', form = form)
--- src/utils/forms.py
+++ src/utils/forms.py
@@ -1,9 +1,12 @@
+# Componentes que se utilizaran
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
+# Clase que contiene los campos del formulario con FlaskForm
 class Registro(FlaskForm) :
     
+    # Cada campo con sus validaciones necesarias
     name = StringField("name", validators = [DataRequired()])
     surname = StringField("surname", validators = [DataRequired()])
     gmail = StringField("gmail", validators = [DataRequired(), Email()])
