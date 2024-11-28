9cc1059407db5f6e96dfc36e5ad993184a2baceb
Nieva Lucas <nievalucas60@gmail.com>
2024-11-28 20:18:18 -0300

    Optimizacion de codigo con Lambda

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,9 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # Funcion con la que generamos un nuevo Hash
-def hash_password(newPassword) :
-    return generate_password_hash(newPassword)
-
+hash_password = lambda newPassword : generate_password_hash(newPassword)
+    
 # Funcion con la que verificamos que la contraseña hasheada y el texto plano son las mismas
-def check_password(encoded, password) :
-    return check_password_hash(encoded, password)
+check_password = lambda encoded, password : check_password_hash(encoded, password)
