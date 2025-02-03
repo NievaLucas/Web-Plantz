0c82948b5d1825264a97083df1638a7aab02fe96
Nieva Lucas <nievalucas60@gmail.com>
2025-02-03 00:26:34 -0300

    Manejo de error en caso de no conectarse a la BBDD

--- src/database/conectDB.py
+++ src/database/conectDB.py
@@ -1,14 +1,19 @@
 from decouple import config
+from flask import abort
 # Modulo conector con MySQL
 import mysql.connector
 
 # Variable con las credenciales para conectarse a la BBDD en la nube
-db = mysql.connector.connect(
 
-    host = config('MYSQL_HOST'),
-    user = config('MYSQL_USER'),
-    password = config('MYSQL_PASSWORD'),
-    port = config('MYSQL_PORT'),
-    database = config('MYSQL_DATABASE')
+try : 
+    db = mysql.connector.connect(
 
-)
+        host = config('MYSQL_HOST'),
+        user = config('MYSQL_USER'),
+        password = config('MYSQL_PASSWORD'),
+        port = config('MYSQL_PORT'),
+        database = config('MYSQL_DATABASE')
+    )
+# En caso de perder conexion con la BBDD, lanzamos un error HTTP 500
+except :
+    abort(500)
